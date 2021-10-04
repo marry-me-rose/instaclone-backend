b80b243b1f0bb9450e42bf4482ba7953070320c3
marry-me-rose <zzangsujin96@gmail.com>
2021-10-04 22:41:52 +0900

    #3 clear

---
+++ schema.js
@@ -0,0 +1,15 @@
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
+
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadedResolvers = loadFilesSync(
+  `${__dirname}/**/*.{queries,mutations}.js`
+);
+
+const typeDefs = mergeTypeDefs(loadedTypes);
+const resolvers = mergeResolvers(loadedResolvers);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export default schema;
---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import schema from "./schema";
+
+const server = new ApolloServer({
+  schema,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground({})],
+});
+
+const PORT = process.env.PORT;
+
+server
+  .listen(PORT)
+  .then(() => console.log("🧀 Server is running on http://localhost:${PORT}/"));
